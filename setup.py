63c18b528d75e850c01befbb17f3993b2bf2b2ad
Mike Kittridge <mgkittridge@gmail.com>
2023-01-13 19:57:34 +1300

    updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 # General parameters
 name = 'booklet'
 main_package = 'booklet'
-version = '0.0.13'
+version = '0.0.14'
 descrip = 'A python key-value file database'
 
 # The below code is for readthedocs. To have sphinx/readthedocs interact with
