792ed7049cf6fc2527eca2d32963864d6f88e3c8
Mike Kittridge <mgkittridge@gmail.com>
2023-01-13 19:51:33 +1300

    fixed

--- booklet/__init__.py
+++ booklet/__init__.py
@@ -1,6 +1,6 @@
 from booklet.main import open, Booklet
 from . import serializers
 
-available_serializers = list(serial_dict.keys())
+available_serializers = list(serializers.serial_dict.keys())
 
 __all__ = ["open", "Booklet", "available_serializers"]
